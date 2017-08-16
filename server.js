2f4be772edbb160549c6142c50c9b2bc0dc1a446
Eric Sowell <eric.sowell@gmail.com>
2017-08-16 09:58:35 -0500

    project setup complete

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mustacheExpress = require('mustache-express');
+require('dotenv').config();
+
+const app = express();
+
+const mustache = mustacheExpress();
+mustache.cache = null;
+app.engine('mustache', mustache);
+app.set('view engine', 'mustache');
+
+app.use(express.static('public'));
+
+app.get('/list', (req, res) => {
+  res.render('list');
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on port ${process.env.PORT}.`);
+});
